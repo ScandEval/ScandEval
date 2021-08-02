5953d78abfd878361b6f8a5484d7b7dc287e8417
saattrupdan <saattrupdan@gmail.com>
2021-08-02 18:30:06 +0100

    fix: Disable tokenizers parallelism

--- scandeval/__init__.py
+++ scandeval/__init__.py
@@ -1,10 +1,17 @@
 __version__ = '0.0.0'  # noqa
 
+# Block unwanted terminal outputs
 from .utils import block_terminal_output; block_terminal_output()
 
+# Set up logging
 import logging
 format = '%(asctime)s [%(levelname)s] <%(name)s> %(message)s'
 logging.basicConfig(level=logging.INFO, format=format)
 
+# Disable parallelisation when tokenizing, as that can lead to errors
+import os
+os.environ['TOKENIZERS_PARALLELISM'] = 'false'
+
+# Import classes
 from .dane import DaneBenchmark  # noqa
 from .benchmark import Benchmark  # noqa
