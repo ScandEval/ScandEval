5b8eaad2909459088e3b4f0ef5bb6494b6366e0d
saattrupdan <saattrupdan@gmail.com>
2021-08-02 20:05:28 +0100

    style: DaNE logging output

--- scandeval/dane.py
+++ scandeval/dane.py
@@ -230,12 +230,12 @@ class DaneBenchmark(BaseBenchmark):
 
         if not np.isnan(train_std_err):
             msg = (f'Mean micro-average F1-scores on DaNE for {model_id}:\n'
-                   f'  Train: {train_mean:.2f} +- {train_std_err:.2f}\n'
-                   f'  Test: {test_mean:.2f} +- {test_std_err:.2f}')
+                   f'  - Train: {train_mean:.2f} +- {train_std_err:.2f}\n'
+                   f'  - Test: {test_mean:.2f} +- {test_std_err:.2f}')
         else:
             msg = (f'Micro-average F1-scores on DaNE for {model_id}:\n'
-                   f'  Train: {train_mean:.2f}\n'
-                   f'  Test: {test_mean:.2f}')
+                   f'  - Train: {train_mean:.2f}\n'
+                   f'  - Test: {test_mean:.2f}')
 
         logger.info(msg)
 
