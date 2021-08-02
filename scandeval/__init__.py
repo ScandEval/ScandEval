31e36cb5914dbeaca2851c069ebea98252ad5389
saattrupdan <saattrupdan@gmail.com>
2021-08-02 16:43:18 +0100

    chore: Add logging to init

--- scandeval/__init__.py
+++ scandeval/__init__.py
@@ -1,3 +1,8 @@
 from .utils import block_terminal_output; block_terminal_output()
+
+import logging
+format = '%(asctime)s [%(levelname)s] <%(name)s> %(message)s'
+logging.basicConfig(level=logging.INFO, format=format)
+
 from .dane import DaneBenchmark  # noqa
 from .benchmark import Benchmark  # noqa
