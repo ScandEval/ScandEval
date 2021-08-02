305366c512b8a37ee64877beb915f4f23d3720e5
saattrupdan <saattrupdan@gmail.com>
2021-08-02 19:55:34 +0100

    style: Add coloured logging

--- scandeval/__init__.py
+++ scandeval/__init__.py
@@ -5,7 +5,9 @@ from .utils import block_terminal_output; block_terminal_output()
 
 # Set up logging
 import logging
-format = '%(asctime)s [%(levelname)s] <%(name)s> %(message)s'
+from termcolor import colored
+format = colored('%(asctime)s [%(levelname)s] <%(name)s>\n', 'green') + \
+         colored('    %(message)s', 'yellow')
 logging.basicConfig(level=logging.INFO, format=format)
 
 # Disable parallelisation when tokenizing, as that can lead to errors
