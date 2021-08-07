cd6360bd796d672e350b308187afacdc5180b9df
saattrupdan <saattrupdan@gmail.com>
2021-08-07 14:35:30 +0100

    style: Change "+-" to proper plus-minus symbol

--- scandeval/angry_tweets.py
+++ scandeval/angry_tweets.py
@@ -94,8 +94,8 @@ class AngryTweetsBenchmark(TextClassificationBenchmark):
         if not np.isnan(train_std_err):
             msg = (f'Mean macro-average F1-scores on AngryTweets for '
                    f'{model_id}:\n'
-                   f'  - Train: {train_mean:.2f} +- {train_std_err:.2f}\n'
-                   f'  - Test: {test_mean:.2f} +- {test_std_err:.2f}')
+                   f'  - Train: {train_mean:.2f} ± {train_std_err:.2f}\n'
+                   f'  - Test: {test_mean:.2f} ± {test_std_err:.2f}')
         else:
             msg = (f'Macro-average F1-scores on AngryTweets for {model_id}:\n'
                    f'  - Train: {train_mean:.2f}\n'
