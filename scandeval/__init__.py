5706baadd847297eea5125c6a7e2959e69f505db
saattrupdan <saattrupdan@gmail.com>
2021-08-18 11:51:04 +0100

    feat: Remove individual benchmarks from init

--- scandeval/__init__.py
+++ scandeval/__init__.py
@@ -2,16 +2,6 @@ from termcolor import colored
 import logging
 import os
 from .benchmark import Benchmark  # noqa
-from .angry_tweets import AngryTweetsBenchmark  # noqa
-from .dane import DaneBenchmark  # noqa
-from .ddt_pos import DdtPosBenchmark  # noqa
-from .ddt_dep import DdtDepBenchmark  # noqa
-from .dkhate import DkHateBenchmark  # noqa
-from .europarl1 import Europarl1Benchmark  # noqa
-from .europarl2 import Europarl2Benchmark  # noqa
-from .lcc1 import Lcc1Benchmark  # noqa
-from .lcc2 import Lcc2Benchmark  # noqa
-from .twitter_sent import TwitterSentBenchmark  # noqa
 from .utils import block_terminal_output
 
 __version__ = '0.7.0'  # noqa
