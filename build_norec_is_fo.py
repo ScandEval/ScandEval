825d2c2162e997c0cdb92ba054157c97aae3e1ad
saattrupdan <saattrupdan@gmail.com>
2021-09-10 12:44:22 +0100

    feat: Add build_norec_is_fo

---
+++ build_norec_is_fo.py
@@ -0,0 +1,68 @@
+'''Build Icelandic and Faroese sentiment datasets by translating NoReC'''
+
+from transformers import pipeline
+from typing import Tuple
+from pathlib import Path
+import json
+from tqdm.auto import tqdm
+
+
+def build_norec_is_fo():
+    '''Translates the NoReC corpus into Icelandic and Norwegian'''
+
+    # Initialise translation pipeline
+    translation_model = 'Helsinki-NLP/opus-mt-NORTH_EU-NORTH_EU'
+    translator = pipeline('translation', translation_model)
+
+    # Initialise translation helper function
+    def translate(doc: str) -> Tuple[str, str]:
+        tr_docs = [f'>>is<< {doc}', f'>>fo<< {doc}']
+        translations = [dct['translation_text'] for dct in translator(tr_docs)]
+        return tuple(translations)
+
+    # Load NoReC
+    norec_dir = Path('datasets/norec')
+    norec_train = (norec_dir / 'train.jsonl').read_text().split('\n')
+    norec_test = (norec_dir / 'test.jsonl').read_text().split('\n')
+
+    # Initialise NoReC-IS and NoReC-FO directories
+    norec_is_dir = Path('datasets/norec_is')
+    if not norec_is_dir.exists():
+        norec_is_dir.mkdir()
+    norec_fo_dir = Path('datasets/norec_fo')
+    if not norec_fo_dir.exists():
+        norec_fo_dir.mkdir()
+
+    # Translate NoReC train
+    for json_line in tqdm(norec_train[:10]):
+        record = json.loads(json_line)
+        is_text, fo_text = translate(record['text'])
+
+        with (norec_is_dir / 'train.jsonl').open('a') as f:
+            is_record = dict(record)
+            is_record['text'] = is_text
+            f.write(json.dumps(is_record) + '\n')
+
+        with (norec_fo_dir / 'train.jsonl').open('a') as f:
+            fo_record = dict(record)
+            fo_record['text'] = fo_text
+            f.write(json.dumps(is_record) + '\n')
+
+    # Translate NoReC test
+    for json_line in tqdm(norec_test[:10]):
+        record = json.loads(json_line)
+        is_text, fo_text = translate(record['text'])
+
+        with (norec_is_dir / 'test.jsonl').open('w') as f:
+            is_record = dict(record)
+            is_record['text'] = is_text
+            f.write(json.dumps(is_record) + '\n')
+
+        with (norec_fo_dir / 'test.jsonl').open('w') as f:
+            fo_record = dict(record)
+            fo_record['text'] = fo_text
+            f.write(json.dumps(is_record) + '\n')
+
+
+if __name__ == '__main__':
+    build_norec_is_fo()
