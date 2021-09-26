a9603dc5e74bf55a8edf6d307b0b461e68e4903c
saattrupdan <saattrupdan@gmail.com>
2021-09-27 01:05:41 +0200

    feat: v1.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,7 @@ and this project adheres to
 [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
-## [Unreleased]
+## [v1.1.2] - 2021-09-27
 ### Fixed
 - Missing dependency parsing tags.
 
--- scandeval/__init__.py
+++ scandeval/__init__.py
@@ -5,7 +5,7 @@ from .benchmark import Benchmark  # noqa
 from .datasets import load_dataset  # noqa
 from .utils import block_terminal_output
 
-__version__ = '1.1.1'  # noqa
+__version__ = '1.1.2'  # noqa
 
 # Block unwanted terminal outputs
 block_terminal_output()
