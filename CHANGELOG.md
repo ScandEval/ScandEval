79270a4042af668754cea978a36e6620f59421e4
saattrupdan <saattrupdan@gmail.com>
2021-11-25 13:19:30 +0000

    feat: v1.3.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,7 @@ and this project adheres to
 [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
-## [Unreleased]
+## [v1.3.7] - 2021-11-25
 ### Fixed
 - Removed `transformers` logging during evaluation as well.
 
--- scandeval/__init__.py
+++ scandeval/__init__.py
@@ -5,7 +5,7 @@ from .benchmark import Benchmark  # noqa
 from .datasets import load_dataset  # noqa
 from .utils import block_terminal_output
 
-__version__ = '1.3.6'  # noqa
+__version__ = '1.3.7'  # noqa
 
 # Block unwanted terminal outputs
 block_terminal_output()
