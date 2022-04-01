4f8ac85bf022effbd27bf663f140d497339d658c
saattrupdan <saattrupdan@gmail.com>
2022-04-01 15:41:54 +0100

    feat: Update the setup requirements, update packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 from bump_version import get_current_version
 
-PYTORCH_REQUIRES = ['torch>=1.6.0']
+PYTORCH_REQUIRES = ['torch>=1.9.0']
 JAX_REQUIRES = PYTORCH_REQUIRES + ['jaxlib>=0.1.75', 'jax>=0.2.26', 'flax>=0.3.6']
 SPACY_REQUIRES = ['spacy>=3.2.0', 'spacy-transformers>=1.1.0']
 ALL_REQUIRES = (PYTORCH_REQUIRES +
@@ -23,15 +23,15 @@ setup(name='scandeval',
                    'Programming Language :: Python :: 3.8'],
       packages=find_packages(exclude=('tests',)),
       include_package_data=True,
-      install_requires=['numpy>=1.19.2',
-                        'transformers>=4.5.0',
-                        'datasets>=1.11.0',
-                        'requests>=2.19.0',
+      install_requires=['numpy>=1.19.5',
+                        'transformers>=4.17.0',
+                        'datasets>=2.0.0',
+                        'requests>=2.26.0',
                         'tqdm>=4.62.3',
                         'sentencepiece>=0.1.96',
-                        'seqeval>=1.0.0',
+                        'seqeval>=1.2.2',
                         'bs4>=0.0.1',
-                        'termcolor>=1.0.0'],
+                        'termcolor>=1.1.0'],
       extras_require=dict(pytorch=PYTORCH_REQUIRES,
                           spacy=SPACY_REQUIRES,
                           jax=JAX_REQUIRES,
