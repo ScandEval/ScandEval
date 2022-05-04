58b56a7fb47b9f230e9d37e7a2e94e6fd4d4fb60
saattrupdan <saattrupdan@gmail.com>
2022-05-04 12:47:24 +0100

    feat: Set patience to 3

--- scandeval/benchmarks/abstract/base.py
+++ scandeval/benchmarks/abstract/base.py
@@ -932,7 +932,7 @@ class BaseBenchmark(ABC):
                         )
 
                         # Initialise early stopping callback
-                        patience = 2 + 1000 // len(train)
+                        patience = 3  # 2 + 1000 // len(train)
                         params = dict(early_stopping_patience=patience)
                         early_stopping = EarlyStoppingCallback(**params)
 
