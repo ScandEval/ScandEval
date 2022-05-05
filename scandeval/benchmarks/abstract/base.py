a01f4a2cdb3e669002b9ab8a440525bb4233514a
saattrupdan <saattrupdan@gmail.com>
2022-05-05 14:52:31 +0100

    feat: Up training size to 32

--- scandeval/benchmarks/abstract/base.py
+++ scandeval/benchmarks/abstract/base.py
@@ -844,7 +844,7 @@ class BaseBenchmark(ABC):
         # Shuffle the training dataset and truncate the training dataset to the
         # first 1000 examples, and the validation set to the following 300
         # samples
-        train_size = 16
+        train_size = 32
         shuffled_train = train.shuffle(seed=4242)
         train = shuffled_train.select(range(train_size))
         val = shuffled_train.select(range(8192, min(8492, len(shuffled_train))))
