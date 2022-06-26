b5c63d91db74eb368b8ff1cc1df359147ca2902c
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2022-06-26 14:48:42 +0200

    fix: Change __init__ in root back to original

--- src/scandeval/__init__.py
+++ src/scandeval/__init__.py
@@ -1,21 +1,28 @@
+"""Evaluation of language models on mono- or multilingual Scandinavian language tasks."""
+
 import logging
 import os
+from importlib import metadata
 
 from termcolor import colored
 
-from .benchmark import Benchmark  # noqa
+from .benchmarker import Benchmarker  # noqa
 from .utils import block_terminal_output
 
-__version__ = "3.0.0"  # noqa
+# Fetches the version of the package as defined in pyproject.toml
+__version__ = metadata.version(__package__)  # noqa
+
 
 # Block unwanted terminal outputs
 block_terminal_output()
+
 
 # Set up logging
 format = colored("%(asctime)s [%(levelname)s] <%(name)s>\n? ", "green") + colored(
     "%(message)s", "yellow"
 )
 logging.basicConfig(level=logging.INFO, format=format)
+
 
 # Disable parallelisation when tokenizing, as that can lead to errors
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
