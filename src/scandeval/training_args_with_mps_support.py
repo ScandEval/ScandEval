49636120304662e4bb1f16219f741e2b02fe5e07
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-07-08 14:49:28 +0200

    docs: Remove commented lines in training_args_with_mps_support

--- src/scandeval/training_args_with_mps_support.py
+++ src/scandeval/training_args_with_mps_support.py
@@ -17,15 +17,6 @@ from transformers.utils import (
 
 
 class TrainingArgumentsWithMPSSupport(TrainingArguments):
-    # @property
-    # def device(self) -> torch.device:
-    #    if torch.cuda.is_available():
-    #        return torch.device("cuda")
-    #    elif torch.backends.mps.is_available():
-    #        return torch.device("mps")
-    #    else:
-    #        return torch.device("cpu")
-
     @cached_property
     @torch_required
     def _setup_devices(self) -> "torch.device":
