675eb331ad13d2f545abd81d1aa9eed8f06cc8b8
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-07-14 13:59:33 +0200

    chore: Create empty dataset creation scripts

---
+++ src/scripts/create_mim_gold_ner.py
@@ -0,0 +1,3 @@
+"""Create the MIM-GOLD-NER-mini NER dataset and upload it to the HF Hub."""
+
+url = "https://repository.clarin.is/repository/xmlui/bitstream/handle/20.500.12537/230/MIM-GOLD-2_0.zip?sequence=15&isAllowed=y"
