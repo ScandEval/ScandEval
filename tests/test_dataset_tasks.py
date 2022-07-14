86e45186c301509a79b2610eaac9d004016b2d46
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-07-14 23:08:02 +0200

    tests: Add unit tests for the dataset_tasks module

---
+++ tests/test_dataset_tasks.py
@@ -0,0 +1,22 @@
+"""Unit tests for the `dataset_tasks` module."""
+
+import pytest
+
+from src.scandeval.config import DatasetTask
+from src.scandeval.dataset_tasks import get_all_dataset_tasks
+
+
+class TestGetAllDatasetTasks:
+    @pytest.fixture(scope="class")
+    def dataset_tasks(self):
+        yield get_all_dataset_tasks()
+
+    def test_dataset_tasks_is_dict(self, dataset_tasks):
+        assert isinstance(dataset_tasks, dict)
+
+    def test_dataset_tasks_are_objects(self, dataset_tasks):
+        for dataset_task in dataset_tasks.values():
+            assert isinstance(dataset_task, DatasetTask)
+
+    def test_get_ner_dataset_task(self, dataset_tasks):
+        assert "ner" in dataset_tasks
