c393f8d9b360320460cdedd278643007eb05ada9
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-07-14 23:08:17 +0200

    tests: Add unit tests for the dataset_configs module

---
+++ tests/test_dataset_configs.py
@@ -0,0 +1,29 @@
+"""Unit tests for the `dataset_configs` module."""
+
+import pytest
+
+from src.scandeval.config import DatasetConfig
+from src.scandeval.dataset_configs import get_all_dataset_configs, get_dataset_config
+
+
+class TestGetAllDatasetConfigs:
+    @pytest.fixture(scope="class")
+    def dataset_configs(self):
+        yield get_all_dataset_configs()
+
+    def test_dataset_configs_is_dict(self, dataset_configs):
+        assert isinstance(dataset_configs, dict)
+
+    def test_dataset_configs_are_objects(self, dataset_configs):
+        for dataset_config in dataset_configs.values():
+            assert isinstance(dataset_config, DatasetConfig)
+
+
+class TestGetDatasetConfig:
+    def test_get_angry_tweets_config(self):
+        dataset_config = get_dataset_config("angry-tweets")
+        assert dataset_config.name == "angry-tweets"
+
+    def test_error_when_dataset_does_not_exist(self):
+        with pytest.raises(ValueError):
+            get_dataset_config("does-not-exist")
