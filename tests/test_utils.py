d3c746a5d5352c1873f384672dba0efefdcee435
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-07-15 14:25:11 +0200

    tests: Add scaffolding for unit tests for the utils module

---
+++ tests/test_utils.py
@@ -0,0 +1,24 @@
+"""Unit tests for the `utils` module."""
+
+from src.scandeval.utils import (
+    block_terminal_output,
+    clear_memory,
+    enforce_reproducibility,
+    is_module_installed,
+)
+
+
+class TestClearMemory:
+    pass
+
+
+class TestEnforceReproducibility:
+    pass
+
+
+class TestIsModuleInstalled:
+    pass
+
+
+class TestBlockTerminalOutput:
+    pass
