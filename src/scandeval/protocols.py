715e0bb5918fd046cbb6a06164faf3af996043fd
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-10-09 14:47:42 +0200

    style: Under-specify arguments in protocol methods

--- src/scandeval/protocols.py
+++ src/scandeval/protocols.py
@@ -1,10 +1,8 @@
 """Protocols used in the project."""
 
-from os import PathLike
-from typing import Callable, Dict, Iterator, List, Protocol, Tuple, Union
+from typing import Dict, Iterator, List, Protocol, Tuple, Union
 
-from datasets.features.features import Features
-from torch import FloatTensor, LongTensor, Tensor
+from torch import FloatTensor, Tensor
 from torch.nn.parameter import Parameter
 
 
@@ -13,11 +11,7 @@ class Config(Protocol):
     label2id: Dict[str, int]
 
     @classmethod
-    def from_pretrained(
-        cls,
-        pretrained_model_name_or_path: Union[str, PathLike, None],
-        **kwargs,
-    ) -> "Config":
+    def from_pretrained(cls, *args, **kwargs) -> "Config":
         ...
 
 
@@ -40,34 +34,17 @@ class Model(Protocol):
         self,
         *args,
         **kwargs,
-        # input_ids: Union[LongTensor, None],
-        # attention_mask: Union[FloatTensor, None],
-        # token_type_ids: Union[LongTensor, None],
-        # position_ids: Union[LongTensor, None],
-        # head_mask: Union[FloatTensor, None],
-        # inputs_embeds: Union[FloatTensor, None],
-        # labels: Union[LongTensor, None],
-        # output_attentions: Union[bool, None],
-        # output_hidden_states: Union[bool, None],
-        # return_dict: Union[bool, None],
     ) -> Union[Tuple[Tensor], ModelOutput]:
         ...
 
-    def parameters(self, recurse: bool = True) -> Iterator[Parameter]:
+    def parameters(self, *args, **kwargs) -> Iterator[Parameter]:
         ...
 
     @classmethod
-    def from_pretrained(
-        cls,
-        *args,
-        **kwargs,
-        # pretrained_model_name_or_path: Union[str, PathLike, None],
-        # *model_args,
-        # **kwargs,
-    ) -> Union[Tuple["Model", Dict], "Model"]:
+    def from_pretrained(cls, *args, **kwargs) -> Union[Tuple["Model", Dict], "Model"]:
         ...
 
 
 class DataCollator(Protocol):
-    def __call__(self, features: List[Dict]) -> Dict:
+    def __call__(self, *args, **kwargs) -> Dict:
         ...
