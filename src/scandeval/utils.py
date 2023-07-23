fbf76681e240c8950af76978b4242535d7e747c4
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-07-23 18:53:00 +0200

    chore: ignore CTRL models for now

--- src/scandeval/utils.py
+++ src/scandeval/utils.py
@@ -500,6 +500,7 @@ def get_huggingface_model_lists(
     BANNED_MODELS = [
         r"TransQuest/siamesetransquest-da.*",
         r"M-CLIP/.*",
+        r".*/.*CTRL.*",
     ]
     for lang, model_list in model_lists.items():
         model_lists[lang] = [
