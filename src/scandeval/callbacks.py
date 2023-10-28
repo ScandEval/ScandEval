58543ce9acbe72183e5ca9d5d3013662ef194d64
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-10-28 21:17:07 +0200

    fix: Change local rank to world rank

--- src/scandeval/callbacks.py
+++ src/scandeval/callbacks.py
@@ -14,7 +14,7 @@ class NeverLeaveProgressCallback(ProgressCallback):
         self.testing = testing
 
     def on_train_begin(self, args, state, control, **kwargs):
-        if state.is_local_process_zero:
+        if state.is_world_process_zero:
             self.training_bar = tqdm(
                 total=None,
                 leave=False,
@@ -24,7 +24,7 @@ class NeverLeaveProgressCallback(ProgressCallback):
         self.current_step = 0
 
     def on_step_end(self, args, state, control, **kwargs):
-        if state.is_local_process_zero:
+        if state.is_world_process_zero:
             self.training_bar.update(state.global_step - self.current_step)
             self.current_step = state.global_step
 
@@ -32,7 +32,7 @@ class NeverLeaveProgressCallback(ProgressCallback):
         if eval_dataloader is None:
             return
         correct_dtype = isinstance(eval_dataloader.dataset, Sized)
-        if state.is_local_process_zero and correct_dtype:
+        if state.is_world_process_zero and correct_dtype:
             if self.prediction_bar is None:
                 desc = "Evaluating model"
                 self.prediction_bar = tqdm(
