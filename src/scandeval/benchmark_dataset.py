248677b59462580c868a33d4dde16f60caa8a84a
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-10-28 17:23:08 +0200

    fix: Disable pbar for non-main processes

--- src/scandeval/benchmark_dataset.py
+++ src/scandeval/benchmark_dataset.py
@@ -390,7 +390,10 @@ class BenchmarkDataset(ABC):
         )
 
         # Prepare the train and validation datasets
-        with tqdm(total=12, desc="Preprocessing data splits", leave=False) as pbar:
+        no_pbar = not self.benchmark_config.is_main_process
+        with tqdm(
+            total=12, desc="Preprocessing data splits", leave=False, disable=no_pbar
+        ) as pbar:
             # When evaluating generative models we only need the test split, so
             # there's no need to prepare the train split
             try:
