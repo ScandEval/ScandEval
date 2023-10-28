ebc770da791ba302d1df8fb77550555166558958
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-10-28 18:22:21 +0200

    debug: Debugging

--- src/scandeval/callbacks.py
+++ src/scandeval/callbacks.py
@@ -24,6 +24,11 @@ class NeverLeaveProgressCallback(ProgressCallback):
             )
         self.current_step = 0
 
+    def on_step_begin(self, args, state, control, **kwargs):
+        print(f"Global step: {state.global_step}")
+        print(f"Current step: {state.current_step}")
+        return super().on_step_begin(args, state, control, **kwargs)
+
     def on_step_end(self, args, state, control, **kwargs):
         print(f"Global step: {state.global_step}")
         print(f"Current step: {state.current_step}")
