8c1b049436ba8a071c60149a816834c9228d26e2
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-10-28 18:27:08 +0200

    chore: Remove prints

--- src/scandeval/callbacks.py
+++ src/scandeval/callbacks.py
@@ -24,15 +24,7 @@ class NeverLeaveProgressCallback(ProgressCallback):
             )
         self.current_step = 0
 
-    def on_step_begin(self, args, state, control, **kwargs):
-        print(f"Global step: {state.global_step}")
-        print(f"Current step: {self.current_step}")
-        return super().on_step_begin(args, state, control, **kwargs)
-
     def on_step_end(self, args, state, control, **kwargs):
-        print(f"Global step: {state.global_step}")
-        print(f"Current step: {self.current_step}")
-
         if state.is_local_process_zero:
             self.training_bar.update(state.global_step - self.current_step)
             self.current_step = state.global_step
