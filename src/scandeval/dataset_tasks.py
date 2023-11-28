77875a7a9a7a76a24dd9131d2cabd8d39efa06d7
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-11-28 12:40:33 +0100

    fix: Use mDeBERTa-v3

--- src/scandeval/dataset_tasks.py
+++ src/scandeval/dataset_tasks.py
@@ -142,7 +142,7 @@ TEXT_TO_TEXT = DatasetTask(
             huggingface_id="bertscore",
             results_key="f1",
             postprocessing_fn=lambda raw_score: (raw_score, f"{raw_score:.2%}"),
-            compute_kwargs=dict(model_type="microsoft/deberta-v3-base", device="cpu"),
+            compute_kwargs=dict(model_type="microsoft/mdeberta-v3-base", device="cpu"),
         ),
         MetricConfig(
             name="rouge-l",
