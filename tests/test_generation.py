f83d10343e3c9c6efe252519e9819a701cf1ace2
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-12-22 23:14:37 +0100

    refactor: Move model cache to separate ModelCache class

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -1,1 +1,41 @@
 """Unit tests for the `generation` module."""
+
+from typing import Generator
+
+import pytest
+from scandeval.generation import (
+    GenerativeModelOutput,
+)
+
+
+@pytest.fixture(scope="module")
+def cache() -> Generator[dict[str, dict[str, GenerativeModelOutput]], None, None]:
+    yield dict()
+
+
+def test_load_model_cache():
+    pass
+
+
+def test_store_cache_to_disk():
+    pass
+
+
+def test_split_dataset_into_cached_and_non_cached():
+    pass
+
+
+def test_load_cached_model_outputs():
+    pass
+
+
+def test_extract_raw_predictions():
+    pass
+
+
+def test_get_generation_stopping_criteria():
+    pass
+
+
+def test_stop_word_criteria():
+    pass
