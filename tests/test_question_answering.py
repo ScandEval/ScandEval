c11bc76691c9d820e2549afcf921b6da18837b79
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-12-26 13:16:45 +0100

    tests: Set up tests for new datasets

--- tests/test_question_answering.py
+++ tests/test_question_answering.py
@@ -7,9 +7,9 @@ import pytest
 from scandeval.benchmark_dataset import BenchmarkDataset
 from scandeval.dataset_configs import (
     GERMANQUAD_CONFIG,
+    NORQUAD_CONFIG,
     NQII_CONFIG,
     SCANDIQA_DA_CONFIG,
-    SCANDIQA_NO_CONFIG,
     SCANDIQA_SV_CONFIG,
     SQUAD_CONFIG,
 )
@@ -21,16 +21,16 @@ from transformers import AutoTokenizer
     scope="module",
     params=[
         SCANDIQA_DA_CONFIG,
-        SCANDIQA_NO_CONFIG,
         SCANDIQA_SV_CONFIG,
+        NORQUAD_CONFIG,
         NQII_CONFIG,
         GERMANQUAD_CONFIG,
         SQUAD_CONFIG,
     ],
     ids=[
         "scandiqa-da",
-        "scandiqa-no",
         "scandiqa-sv",
+        "norquad",
         "nqii",
         "germanquad",
         "squad",
