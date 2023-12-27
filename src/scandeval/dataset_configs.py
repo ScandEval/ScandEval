d6469c1b564ab5dcb734ce98dae38d4d180dc6dc
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-12-27 14:24:55 +0100

    feat: Add MMLU for sv, de and nl

--- src/scandeval/dataset_configs.py
+++ src/scandeval/dataset_configs.py
@@ -712,6 +712,49 @@ MMLU_DA_CONFIG = DatasetConfig(
     max_generated_tokens=3,
 )
 
+MMLU_SV_CONFIG = DatasetConfig(
+    name="mmlu-sv",
+    pretty_name="the Swedish part of the truncated version of MMLU",
+    huggingface_id="ScandEval/mmlu-sv-mini",
+    task=MULTIPLE_CHOICE,
+    languages=[SV],
+    prompt_prefix="",
+    prompt_template="{text}\nSvar: {label}",
+    prompt_label_mapping=dict(a="a", b="b", c="c", d="d"),
+    num_few_shot_examples=5,
+    max_generated_tokens=3,
+)
+
+MMLU_DE_CONFIG = DatasetConfig(
+    name="mmlu-de",
+    pretty_name="the German part of the truncated version of MMLU",
+    huggingface_id="ScandEval/mmlu-de-mini",
+    task=MULTIPLE_CHOICE,
+    languages=[DE],
+    prompt_prefix="",
+    prompt_template="{text}\nAntwort: {label}",
+    prompt_label_mapping=dict(a="a", b="b", c="c", d="d"),
+    num_few_shot_examples=5,
+    max_generated_tokens=3,
+)
+
+MMLU_NL_CONFIG = DatasetConfig(
+    name="mmlu-nl",
+    pretty_name="the Dutch part of the truncated version of MMLU",
+    huggingface_id="ScandEval/mmlu-nl-mini",
+    task=MULTIPLE_CHOICE,
+    languages=[NL],
+    prompt_prefix="",
+    prompt_template="{text}\nAntwoord: {label}",
+    prompt_label_mapping=dict(a="a", b="b", c="c", d="d"),
+    num_few_shot_examples=5,
+    max_generated_tokens=3,
+)
+
+# TODO: Norwegian multiple choice
+# TODO: Icelandic multiple choice
+# TODO: Faroese multiple choice
+
 
 ### SPEED ESTIMATION DATASETS ###
 
