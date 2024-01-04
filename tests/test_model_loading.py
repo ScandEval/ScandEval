d1a316a63bc1effd8f7c61c354509b7b70b1607f
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2024-01-04 16:40:43 +0100

    fix: remove unnecessary tests, already tested elsewhere

--- tests/test_model_loading.py
+++ tests/test_model_loading.py
@@ -35,27 +35,3 @@ def test_load_model(model_id, dataset_config, benchmark_config):
                 dataset_config=dataset_config,
                 benchmark_config=benchmark_config,
             )
-
-
-def test_load_model_with_invalid_model_id(benchmark_config):
-    model_config = get_model_config(
-        model_id="invalid_model_id", benchmark_config=benchmark_config
-    )
-    with pytest.raises(InvalidBenchmark):
-        load_model(
-            model_config=model_config,
-            dataset_config=SPEED_CONFIG,
-            benchmark_config=benchmark_config,
-        )
-
-
-def test_load_model_with_invalid_model_type(benchmark_config):
-    model_config = get_model_config(
-        model_id="invalid_model_type", benchmark_config=benchmark_config
-    )
-    with pytest.raises(InvalidBenchmark):
-        load_model(
-            model_config=model_config,
-            dataset_config=SPEED_CONFIG,
-            benchmark_config=benchmark_config,
-        )
