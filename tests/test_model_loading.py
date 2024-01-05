514a1e9139f9c0d8d047cf6b2b73ce374b88147d
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2024-01-05 13:45:40 +0100

    tests: create seperate tests instead of involved parametrization

--- tests/test_model_loading.py
+++ tests/test_model_loading.py
@@ -1,37 +1,82 @@
 """Unit tests for the `model_loading` module."""
 
 import pytest
-from scandeval.dataset_configs import MMLU_DA_CONFIG, SPEED_CONFIG
+from scandeval.config import ModelConfig
 from scandeval.exceptions import InvalidBenchmark
 from scandeval.model_config import get_model_config
 from scandeval.model_loading import load_model
-from scandeval.utils import GENERATIVE_DATASET_SUPERTASKS, GENERATIVE_DATASET_TASKS
 
 
-@pytest.mark.parametrize(
-    argnames="dataset_config",
-    argvalues=[SPEED_CONFIG, MMLU_DA_CONFIG],
-    ids=["non-generative", "generative"],
-)
-def test_load_model(model_id, dataset_config, benchmark_config):
+def test_load_non_generative_model(model_id, dataset_config, benchmark_config):
     model_config = get_model_config(
         model_id=model_id, benchmark_config=benchmark_config
     )
-    if not (
-        dataset_config.task.supertask in GENERATIVE_DATASET_SUPERTASKS
-        or dataset_config.task.name in GENERATIVE_DATASET_TASKS
-    ):
-        tokenizer, model = load_model(
+    tokenizer, model = load_model(
+        model_config=model_config,
+        dataset_config=dataset_config,
+        benchmark_config=benchmark_config,
+    )
+    assert tokenizer is not None
+    assert model is not None
+
+
+def test_load_generative_model(
+    generative_model_id, generative_dataset_config, benchmark_config
+):
+    model_config = get_model_config(
+        model_id=generative_model_id, benchmark_config=benchmark_config
+    )
+    tokenizer, model = load_model(
+        model_config=model_config,
+        dataset_config=generative_dataset_config,
+        benchmark_config=benchmark_config,
+    )
+    assert tokenizer is not None
+    assert model is not None
+
+
+def test_load_non_generative_model_with_generative_data(
+    model_id, generative_dataset_config, benchmark_config
+):
+    model_config = get_model_config(
+        model_id=model_id, benchmark_config=benchmark_config
+    )
+    with pytest.raises(InvalidBenchmark):
+        load_model(
+            model_config=model_config,
+            dataset_config=generative_dataset_config,
+            benchmark_config=benchmark_config,
+        )
+
+
+def test_load_generative_model_with_non_generative_data(
+    generative_model_id, dataset_config, benchmark_config
+):
+    model_config = get_model_config(
+        model_id=generative_model_id, benchmark_config=benchmark_config
+    )
+    tokenizer, model = load_model(
+        model_config=model_config,
+        dataset_config=dataset_config,
+        benchmark_config=benchmark_config,
+    )
+    assert tokenizer is not None
+    assert model is not None
+
+
+def test_load_non_existing_model(dataset_config, benchmark_config):
+    model_config = ModelConfig(
+        model_id="non-existing-model",
+        revision="revision",
+        framework="framework",
+        task="task",
+        languages=["language"],
+        model_type="fresh",
+        model_cache_dir="cache_dir",
+    )
+    with pytest.raises(InvalidBenchmark):
+        load_model(
             model_config=model_config,
             dataset_config=dataset_config,
             benchmark_config=benchmark_config,
         )
-        assert tokenizer is not None
-        assert model is not None
-    else:
-        with pytest.raises(InvalidBenchmark):
-            load_model(
-                model_config=model_config,
-                dataset_config=dataset_config,
-                benchmark_config=benchmark_config,
-            )
