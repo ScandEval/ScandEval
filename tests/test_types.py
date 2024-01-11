846bb06159adbb6144dea9dcb6f93ca5220091af
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-01-11 14:52:24 +0100

    tests: Changes to `typing` module tests

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,5 +1,6 @@
 """Unit tests for the `types` module."""
 
+import re
 import typing
 from typing import Generator
 
@@ -10,16 +11,15 @@ import scandeval.types as types
 @pytest.fixture(scope="module")
 def module_variable_names() -> Generator[list[str], None, None]:
     """Yields the module variable names."""
-    yield [var for var in dir(types) if "_" not in var and not hasattr(typing, var)]
+    yield [
+        var
+        for var in dir(types)
+        if "_" not in var and not hasattr(typing, var) and var != "np"
+    ]
 
 
-def test_variable_names_are_upper_case(module_variable_names) -> None:
-    """Tests that all module variable names are upper case."""
+def test_type_variable_names_are_title_case(module_variable_names) -> None:
+    """Tests that all type variable names are title case."""
     for var in module_variable_names:
-        assert var.isupper()
-
-
-def test_variables_subclass_typing_types(module_variable_names) -> None:
-    """Test that all module variable names are attributes of the `typing` module."""
-    for var in module_variable_names:
-        assert hasattr(typing, type(var).__name__)
+        assert var[0].isupper()
+        assert re.search(r"[^A-Za-z]", var) is None
