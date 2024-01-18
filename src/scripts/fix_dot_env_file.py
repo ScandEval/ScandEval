548f49ded42f055646a98171421f3012fbc7cd9b
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-01-18 15:23:52 +0100

    chore: Remove GPG

--- src/scripts/fix_dot_env_file.py
+++ src/scripts/fix_dot_env_file.py
@@ -1,13 +1,9 @@
 """Checks related to the .env file in the repository."""
 
-import subprocess
 from pathlib import Path
 
 # List of all the environment variables that are desired
 DESIRED_ENVIRONMENT_VARIABLES = dict(
-    GPG_KEY_ID="Enter GPG key ID or leave empty if you do not want to use it. Type "
-    "`gpg --list-secret-keys --keyid-format=long | grep sec | sed -E "
-    "'s/.*\/([^ ]+).*/\\1/'` to see your key ID:\n> ",  # noqa
     GIT_NAME="Enter your full name, to be shown in Git commits:\n> ",
     GIT_EMAIL="Enter your email, as registered on your Github account:\n> ",
     PYPI_API_TOKEN="Enter your PyPI API token, or leave empty if you do not want "
@@ -41,24 +37,6 @@ def fix_dot_env_file():
     with env_file_path.open("a") as f:
         for env_var in env_vars_missing:
             value = ""
-            if env_var == "GPG_KEY_ID":
-                gpg = subprocess.Popen(
-                    ["gpg", "--list-secret-keys", "--keyid-format=long"],
-                    stdout=subprocess.PIPE,
-                )
-                grep = subprocess.Popen(
-                    ["grep", "sec"], stdin=gpg.stdout, stdout=subprocess.PIPE
-                )
-                value = (
-                    subprocess.check_output(
-                        ["sed", "-E", "s/.*\\/([^ ]+).*/\\1/"],
-                        stdin=grep.stdout,
-                    )
-                    .decode()
-                    .strip("\n")
-                )
-                gpg.wait()
-                grep.wait()
             if value == "":
                 value = input(DESIRED_ENVIRONMENT_VARIABLES[env_var])
             f.write(f'{env_var}="{value}"\n')
