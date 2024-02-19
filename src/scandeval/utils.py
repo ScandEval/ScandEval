31ccb427e29a3d0102983828a81d19934ef26902
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-02-19 17:09:38 +0100

    debug: Try using _worker_logs_enabled

--- src/scandeval/utils.py
+++ src/scandeval/utils.py
@@ -181,7 +181,10 @@ def block_terminal_output():
     logging.getLogger("vllm.model_executor.weight_utils").setLevel(logging.CRITICAL)
     logging.getLogger("httpx").setLevel(logging.CRITICAL)
 
-    logging.getLogger("ray").setLevel(logging.CRITICAL)
+    # logging.getLogger("ray").setLevel(logging.CRITICAL)
+    import ray
+
+    ray._private.worker._worker_logs_enabled = False
 
     # The `lmformatenforcer` uses the root logger, so we need to set the level of that
     logging.getLogger("root").setLevel(logging.CRITICAL)
