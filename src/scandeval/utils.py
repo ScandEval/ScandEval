7c4a81e1d63e6a33b06b0784f145bb04876eef2c
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-02-28 12:33:15 +0100

    fix: Typo in regex

--- src/scandeval/utils.py
+++ src/scandeval/utils.py
@@ -608,7 +608,7 @@ def get_ner_schema(dataset_config: DatasetConfig) -> str:  # type[BaseModel]:
     regex = (
         r"\{"
         r'("(' + "|".join(tag_names) + r')": ?'
-        r'\[("[^"]+"\)(, ?"[^"]+"){0,4}\])+'
+        r'\[("[^"]+")(, ?"[^"]+"){0,4}\])+'
         r"\}"
     )
 
