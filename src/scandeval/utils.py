4e6d743699698724a16a7d5b863505535849d753
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-02-28 12:26:56 +0100

    fix: Regex

--- src/scandeval/utils.py
+++ src/scandeval/utils.py
@@ -607,8 +607,8 @@ def get_ner_schema(dataset_config: DatasetConfig) -> str:  # type[BaseModel]:
     tag_names = set(dataset_config.prompt_label_mapping.values())
     regex = (
         r"\{"
-        r"(\"(" + "|".join(tag_names) + r")\": "
-        r"\[(\"[^\"]+\"\)(,\"[^\"]+\"){0,4}\])+"
+        r"(\"(" + "|".join(tag_names) + r')": ?'
+        r'\[("[^"]+"\)(, ?"[^"]+"){0,4}\])+'
         r"\}"
     )
 
