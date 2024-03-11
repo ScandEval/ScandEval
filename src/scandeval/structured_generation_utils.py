94f8a4ba783ddaac59ccf1569d56dc56492fb980
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-03-11 17:06:05 +0100

    fix: Include outlines-transformers integration directly in the code

--- src/scandeval/structured_generation_utils.py
+++ src/scandeval/structured_generation_utils.py
@@ -2,18 +2,22 @@
 
 import importlib.util
 import math
-from typing import TYPE_CHECKING, Any, Callable
+from collections import defaultdict
+from typing import TYPE_CHECKING, Any, Callable, DefaultDict, Type
 
 import torch
+from outlines.fsm.fsm import FSMState, RegexFSM
+from outlines.fsm.json_schema import build_regex_from_schema
+from outlines.integrations.utils import adapt_tokenizer, convert_json_schema_to_str
 from pydantic import BaseModel, conlist, create_model
-from transformers import PreTrainedTokenizerBase
+from transformers import Pipeline, PreTrainedTokenizerBase
 
 if importlib.util.find_spec("outlines") is not None:
-    from outlines.integrations.transformers import JSONPrefixAllowedTokens
+    # from outlines.integrations.transformers import JSONPrefixAllowedTokens
     from outlines.integrations.vllm import JSONLogitsProcessor
 
 if TYPE_CHECKING:
-    from outlines.integrations.transformers import JSONPrefixAllowedTokens
+    # from outlines.integrations.transformers import JSONPrefixAllowedTokens
     from vllm import LLM
 
 
@@ -33,109 +37,6 @@ def get_ner_schema(ner_tag_names: list[str]) -> type[BaseModel]:
     }
     schema = create_model("AnswerFormat", **keys_and_their_types)
     return schema
-
-
-# class JSONPrefixAllowedTokens:
-#     """Class to be used in `PreTrainedModel.generate` to allow tokens based on JSON."""
-#
-#     def __init__(
-#         self,
-#         ner_tag_names: list[str],
-#         tokenizer: PreTrainedTokenizerBase,
-#         forbidden_token_ids: list[int] = list(),
-#     ):
-#         """Compile the FSM that drives the JSON-guided generation.
-#
-#         Args:
-#             ner_tag_names:
-#                 The NER tag names.
-#             tokenizer:
-#                 The tokenizer to use for tokenizing the JSON Schema.
-#             forbidden_token_ids:
-#                 The token IDs that are forbidden to be generated.
-#         """
-#         if importlib.util.find_spec("outlines") is None:
-#             raise NeedsExtraInstalled(extra="generative")
-#
-#         schema = get_ner_schema(ner_tag_names=ner_tag_names)
-#         schema_str = json.dumps(schema.model_json_schema())
-#         regex_string = build_regex_from_schema(
-#             schema=schema_str, whitespace_pattern=r" ?"
-#         )
-#         tokenizer = self.adapt_tokenizer(tokenizer=tokenizer)
-#         fsm = RegexFSM(regex_string, tokenizer)
-#         self.forbidden_token_ids = forbidden_token_ids
-#         self.fsm = fsm
-#
-#     def __call__(self, batch_id: int, sent: torch.Tensor) -> list[int]:
-#         """Use the FSM to get the allowed tokens for the next token.
-#
-#         Args:
-#             batch_id:
-#                 The batch ID.
-#             sent:
-#                 The input tensor.
-#
-#         Returns:
-#             The allowed tokens.
-#         """
-#         sent = sent.tolist()
-#         seq_id = hash(tuple(sent))
-#
-#         if len(sent) == 0:
-#             self.fsm_state: DefaultDict[int, int] = defaultdict(int)
-#         else:
-#             last_token = sent[-1]
-#             last_seq_id = hash(tuple(sent[:-1]))
-#             self.fsm_state[seq_id] = self.fsm.next_state(
-#                 self.fsm_state[last_seq_id], last_token
-#             )
-#
-#         allowed_tokens = [
-#             token_id
-#             for token_id in self.fsm.allowed_token_ids(self.fsm_state[seq_id])
-#             if token_id not in self.forbidden_token_ids
-#         ]
-#         return allowed_tokens
-#
-#     @staticmethod
-#     def adapt_tokenizer(tokenizer: PreTrainedTokenizerBase) -> PreTrainedTokenizerBase:
-#         """Adapt vLLM's tokenizer to use to compile the FSM.
-#
-#         The API of Outlines tokenizers is slightly different to that of
-#         `transformers`. In addition we need to handle the missing spaces to
-#         Llama's tokenizer to be able to compile FSMs for this model.
-#
-#         Args:
-#             tokenizer:
-#                 The tokenizer to adapt.
-#
-#         Returns:
-#             The adapted tokenizer.
-#         """
-#         tokenizer.vocabulary = tokenizer.get_vocab()
-#         tokenizer.special_tokens = set(tokenizer.all_special_tokens)
-#
-#         def convert_token_to_string(token: str) -> str:
-#             """Patched way to convert a token to a string.
-#
-#             Args:
-#                 token:
-#                     The token to convert.
-#
-#             Returns:
-#                 The string representation of the token.
-#             """
-#             string = tokenizer.convert_tokens_to_string([token])
-#
-#             # A hack to handle missing spaces to HF's Llama tokenizers
-#             if token.startswith(SPIECE_UNDERLINE) or token == "<0x20>":
-#                 return " " + string
-#
-#             return string
-#
-#         tokenizer.convert_token_to_string = convert_token_to_string
-#         return tokenizer
 
 
 def get_ner_prefix_allowed_tokens_fn(
@@ -227,3 +128,115 @@ def get_ner_logits_processors(
     logits_processors.append(no_tabs_or_newlines)
 
     return logits_processors
+
+
+class RegexPrefixAllowedTokens:
+    """Bias transformers generation based on a regular expression.
+
+    Attributes:
+        fsm:
+            The finite state machine which is used to bias the logits.
+    """
+
+    def __init__(
+        self, regex_string: str, tokenizer_or_pipe: PreTrainedTokenizerBase | Pipeline
+    ):
+        """Compile the FSM that drives the regex-structured generation.
+
+        Args:
+            regex_string:
+                A string that represents a regular expression.
+            tokenizer_or_pipe:
+                The tokenizer of the model, or the pipeline object.
+
+        Raises:
+            ValueError:
+                If the `tokenizer_or_pipe` parameter is not a tokenizer or a pipeline.
+        """
+        if isinstance(tokenizer_or_pipe, Pipeline):
+            tokenizer = tokenizer_or_pipe.tokenizer
+        elif isinstance(tokenizer_or_pipe, PreTrainedTokenizerBase):
+            tokenizer = tokenizer_or_pipe
+        # else:
+        #     raise ValueError(
+        #         "The tokenizer_or_pipe parameter must be a tokenizer or a pipeline."
+        #     )
+        # assert isinstance(tokenizer, PreTrainedTokenizerBase)
+        tokenizer = adapt_tokenizer(tokenizer=tokenizer)
+        self.fsm = RegexFSM(regex_string=regex_string, tokenizer=tokenizer)
+        self._fsm_state: DefaultDict[int, int] = defaultdict(int)
+
+        # The generated text with `transformers` include the input token IDs as well,
+        # so we use this attribute to keep track of the input token IDs. This allows us
+        # to reset the FSM state when the input token IDs change, as well as to only
+        # apply the FSM to the generated tokens.
+        self._prefix = [-1]
+
+    def __call__(self, batch_id: int, sent: torch.Tensor) -> list[int]:
+        """Use the FSM to bias the logits before sampling the next token.
+
+        Args:
+            batch_id:
+                The index of the current batch.
+            sent:
+                The tokens of the current sentence.
+
+        Returns:
+            The indices of the tokens that are allowed to be sampled next.
+        """
+        input_ids = sent.tolist()
+
+        # If the prefix token IDs have changed we assume that we are dealing with a new
+        # sample and reset the FSM state
+        if input_ids[: len(self._prefix)] != self._prefix:
+            self._fsm_state = defaultdict(int)
+            self._prefix = input_ids
+            seq_id = hash(tuple([]))
+
+        else:
+            # Remove the prefix token IDs from the input token IDs, as the FSM should
+            # only be applied to the generated tokens
+            input_ids = input_ids[len(self._prefix) :]
+
+            last_token = input_ids[-1]
+            last_seq_id = hash(tuple(input_ids[:-1]))
+            seq_id = hash(tuple(input_ids))
+            self._fsm_state[seq_id] = self.fsm.next_state(
+                state=FSMState(self._fsm_state[last_seq_id]), token_id=last_token
+            )
+
+        allowed_tokens = self.fsm.allowed_token_ids(
+            state=FSMState(self._fsm_state[seq_id])
+        )
+        return allowed_tokens
+
+
+class JSONPrefixAllowedTokens(RegexPrefixAllowedTokens):
+    """Bias transformers generation based on a JSON schema.
+
+    Attributes:
+        fsm:
+            The finite state machine which is used to bias the logits.
+    """
+
+    def __init__(
+        self,
+        schema: dict | Type[BaseModel] | str,
+        tokenizer_or_pipe: PreTrainedTokenizerBase | Pipeline,
+        whitespace_pattern: str | None = None,
+    ):
+        r"""Compile the FSM that drives the JSON-guided generation.
+
+        Args:
+            schema:
+                A schema that encodes the structure we want the model to generate.
+            tokenizer_or_pipe:
+                The tokenizer of the model, or the pipeline object.
+            whitespace_pattern:
+                Pattern to use for JSON syntactic whitespace (doesn't impact string
+                literals). For example, to allow only a single space or newline with
+                `whitespace_pattern=r"[\n ]?"`
+        """
+        schema_str = convert_json_schema_to_str(json_schema=schema)
+        regex_string = build_regex_from_schema(schema_str, whitespace_pattern)
+        super().__init__(regex_string=regex_string, tokenizer_or_pipe=tokenizer_or_pipe)
