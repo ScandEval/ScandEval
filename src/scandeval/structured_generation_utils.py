f3f62042e66f8f0dcae5c87b0c4523e2680f9a80
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-03-18 15:44:58 +0100

    fix: Do not use any forbidden tokens in structured_generation_utils

--- src/scandeval/structured_generation_utils.py
+++ src/scandeval/structured_generation_utils.py
@@ -47,37 +47,11 @@ def get_ner_prefix_allowed_tokens_fn(
     Returns:
         The prefix allowed tokens function for the NER task.
     """
-    schema = get_ner_schema(ner_tag_names=ner_tag_names)
-    json_generation_prefix_allowed_tokens_fn = JSONPrefixAllowedTokens(
-        schema=schema, tokenizer_or_pipe=tokenizer, whitespace_pattern=r" ?"
+    return JSONPrefixAllowedTokens(
+        schema=get_ner_schema(ner_tag_names=ner_tag_names),
+        tokenizer_or_pipe=tokenizer,
+        whitespace_pattern=r" ?",
     )
-
-    forbidden_token_ids = list()
-    forbidden_tokens = ["\n", "\n\n", "\n\n\n", "\t", "\t\t", "\t\t\t"]
-    for forbidden_token in forbidden_tokens:
-        forbidden_token_ids.extend(
-            list(tokenizer(forbidden_token, add_special_tokens=False).input_ids)
-        )
-    forbidden_token_ids = list(set(forbidden_token_ids))
-
-    def prefix_allowed_tokens_fn(batch_id: int, sent: torch.Tensor) -> list[int]:
-        """Functions used to bias the generation of a `transformers` model.
-
-        Args:
-            batch_id:
-                The batch ID.
-            sent:
-                The input tensor.
-
-        Returns:
-            The allowed tokens.
-        """
-        allowed_tokens = json_generation_prefix_allowed_tokens_fn(
-            batch_id=batch_id, sent=sent
-        )
-        return [token for token in allowed_tokens if token not in forbidden_token_ids]
-
-    return prefix_allowed_tokens_fn
 
 
 def get_ner_logits_processors(
@@ -94,31 +68,9 @@ def get_ner_logits_processors(
     Returns:
         The logit processors for the NER task.
     """
-    logits_processors = list()
-
-    # Add JSON generation constraint if we are benchmarking the NER task
-    schema = get_ner_schema(ner_tag_names=ner_tag_names)
     logits_processor = JSONLogitsProcessor(
-        schema=schema, llm=llm, whitespace_pattern=r" ?"
+        schema=get_ner_schema(ner_tag_names=ner_tag_names),
+        llm=llm,
+        whitespace_pattern=r" ?",
     )
-    logits_processors.append(logits_processor)
-
-    # forbidden_token_ids = list()
-    # forbidden_tokens = ["\n", "\n\n", "\n\n\n", "\t", "\t\t", "\t\t\t"]
-    # for forbidden_token in forbidden_tokens:
-    #     forbidden_token_ids.extend(
-    #         list(
-    #             llm.get_tokenizer()(forbidden_token, add_special_tokens=False).input_ids
-    #         )
-    #     )
-    # forbidden_token_ids = list(set(forbidden_token_ids))
-
-    # def no_tabs_or_newlines(_: list[int], scores: torch.Tensor) -> torch.Tensor:
-    #     mask = torch.zeros_like(scores)
-    #     for forbidden_token_id in forbidden_token_ids:
-    #         mask[forbidden_token_id] = -math.inf
-    #     return scores + mask
-
-    # logits_processors.append(no_tabs_or_newlines)
-
-    return logits_processors
+    return [logits_processor]
