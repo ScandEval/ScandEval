d6a955e23138dd369a06a600a9d76bc3983d237f
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-03-25 14:30:06 +0100

    debug: Double quant

--- src/scandeval/model_setups/hf.py
+++ src/scandeval/model_setups/hf.py
@@ -262,8 +262,7 @@ class HFModelSetup:
         bnb_config = (
             BitsAndBytesConfig(
                 load_in_4bit=load_in_4bit,
-                bnb_4bit_quant_type="nf4",  # "fp4" or "nf4"
-                # bnb_4bit_use_double_quant=True,
+                bnb_4bit_use_double_quant=True,
                 # bnb_4bit_compute_dtype=torch.bfloat16 if use_bf16 else torch.float16,
             )
             if load_in_4bit
