dc930e300884a720a64cf8c32f42745c958a7306
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-03-25 12:55:20 +0100

    debug: Use 4bit again

--- src/scandeval/model_setups/hf.py
+++ src/scandeval/model_setups/hf.py
@@ -262,9 +262,8 @@ class HFModelSetup:
         )
         bnb_config = (
             BitsAndBytesConfig(
-                # load_in_4bit=load_in_4bit,
-                # bnb_4bit_use_double_quant=True,
-                load_in_8bit=True,
+                load_in_4bit=load_in_4bit,
+                bnb_4bit_use_double_quant=True,
                 bnb_4bit_compute_dtype=torch.bfloat16 if use_bf16 else torch.float16,
             )
             if load_in_4bit
