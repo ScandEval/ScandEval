b23bb085a1f7c5ccd42da38765c9165d5957fe0f
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-04-04 15:03:36 +0200

    debug

--- src/scandeval/structured_generation_utils.py
+++ src/scandeval/structured_generation_utils.py
@@ -10,6 +10,10 @@ if importlib.util.find_spec("outlines") is not None:
     from outlines.integrations.transformers import JSONPrefixAllowedTokens
     from outlines.integrations.vllm import JSONLogitsProcessor
 
+    # TEMP
+    from outlines.fsm.json_schema import build_regex_from_schema
+    from outlines.integrations.utils import convert_json_schema_to_str
+
 if TYPE_CHECKING:
     from transformers import PreTrainedTokenizerBase
     from vllm import LLM
@@ -25,7 +29,13 @@ def get_ner_schema(ner_tag_names: list[str]) -> type[BaseModel]:
     Returns:
         The schema for the NER answer format.
     """
-    ner_tag_names = sorted(set(ner_tag_names))
+    # TODO: Maybe we need to keep the order, if this affects performance
+    unique_ner_tag_names = list()
+    for tag_name in ner_tag_names:
+        if tag_name not in unique_ner_tag_names:
+            unique_ner_tag_names.append(tag_name)
+    # ner_tag_names = sorted(set(ner_tag_names))
+
     keys_and_their_types: dict[str, Any] = {
         tag_name: (conlist(str, max_length=5), ...) for tag_name in ner_tag_names
     }
@@ -68,6 +78,12 @@ def get_ner_logits_processors(
     Returns:
         The logit processors for the NER task.
     """
+    # TEMP
+    schema = get_ner_schema(ner_tag_names=ner_tag_names)
+    schema_str = convert_json_schema_to_str(json_schema=schema)
+    regex_string = build_regex_from_schema(schema_str, r" ?")  # noqa
+    breakpoint()
+
     logits_processor = JSONLogitsProcessor(
         schema=get_ner_schema(ner_tag_names=ner_tag_names),
         llm=llm,
