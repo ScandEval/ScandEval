6e8cb142db379fafd143265daa9b092286a2673e
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-04-17 13:34:46 +0200

    fix: Only import from `openai` if `openai` is installed

--- src/scandeval/openai_models.py
+++ src/scandeval/openai_models.py
@@ -5,7 +5,6 @@ import logging
 from typing import TYPE_CHECKING, Literal
 
 import torch
-from openai.types.chat.completion_create_params import ResponseFormat
 from torch import LongTensor
 from torch.nn.utils.rnn import pad_sequence
 from transformers import BatchEncoding, GenerationConfig
@@ -25,6 +24,7 @@ if importlib.util.find_spec("tiktoken") is not None:
 
 if importlib.util.find_spec("openai") is not None:
     from openai import AzureOpenAI, BadRequestError, NotFoundError, OpenAI
+    from openai.types.chat.completion_create_params import ResponseFormat
 
 logger = logging.getLogger(__package__)
 
