cf7c4d0b3d28c31be703c74cb5ad46b59177323c
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-04-19 19:22:29 +0200

    fix: Set `num_cpus` in `ray.init`

--- src/scandeval/vllm_models.py
+++ src/scandeval/vllm_models.py
@@ -21,11 +21,12 @@ if TYPE_CHECKING:
     from pathlib import Path
 
     from transformers import PretrainedConfig, PreTrainedTokenizerBase
-    from vllm import LLM, RequestOutput
+    from vllm import LLM, ParallelConfig, RequestOutput
 
     from .config import DatasetConfig, ModelConfig
 
 if importlib.util.find_spec("vllm") is not None:
+    import vllm
     from vllm import LLM, SamplingParams
     from vllm.model_executor.parallel_utils.parallel_state import destroy_model_parallel
 
@@ -127,6 +128,10 @@ class VLLMModel:
             max_logprobs=10,
             enable_prefix_caching=True,
         )
+
+        # TEMP: Fix issue with vLLM + Ray re-initialisation. See this vLLM issue:
+        # https://github.com/vllm-project/vllm/issues/1058
+        vllm.engine.ray_utils.initialize_ray_cluster = _initialize_ray_cluster
 
         # TEMP: We do a try-except here since some arguments are introduced in vLLM
         # v0.4.0, and we want to be able to use older versions of vLLM as well (for
@@ -384,3 +389,65 @@ def _run_engine_with_fixed_progress_bars(
     outputs = sorted(outputs, key=lambda x: int(x.request_id))
 
     return outputs
+
+
+def _initialize_ray_cluster(
+    parallel_config: "ParallelConfig", ray_address: str | None = None
+):
+    """Initialize the distributed cluster with Ray.
+
+    it will connect to the Ray cluster and create a placement group
+    for the workers, which includes the specification of the resources
+    for each distributed worker.
+
+    Args:
+        parallel_config: The configurations for parallel execution.
+        ray_address: The address of the Ray cluster. If None, uses
+            the default Ray cluster address.
+    """
+    import multiprocessing as mp
+
+    import ray
+
+    # Connect to a ray cluster.
+    ray.init(address=ray_address, ignore_reinit_error=True, num_cpus=mp.cpu_count() - 1)
+
+    if parallel_config.placement_group:
+        # Placement group is already set.
+        return
+
+    # Create placement group for worker processes
+    current_placement_group = ray.util.get_current_placement_group()
+    if current_placement_group:
+        # We are in a placement group
+        bundles = current_placement_group.bundle_specs
+        # Verify that we can use the placement group.
+        gpu_bundles = 0
+        for bundle in bundles:
+            bundle_gpus = bundle.get("GPU", 0)
+            if bundle_gpus > 1:
+                raise ValueError("Placement group bundle cannot have more than 1 GPU.")
+            if bundle_gpus:
+                gpu_bundles += 1
+        if parallel_config.world_size > gpu_bundles:
+            raise ValueError(
+                "The number of required GPUs exceeds the total number of "
+                "available GPUs in the placement group."
+            )
+    else:
+        num_gpus_in_cluster = ray.cluster_resources().get("GPU", 0)
+        if parallel_config.world_size > num_gpus_in_cluster:
+            raise ValueError(
+                "The number of required GPUs exceeds the total number of "
+                "available GPUs in the cluster."
+            )
+        # Create a new placement group
+        placement_group_specs = [{"GPU": 1}] * parallel_config.world_size
+        current_placement_group = ray.util.placement_group(placement_group_specs)
+        # Wait until PG is ready - this will block until all
+        # requested resources are available, and will timeout
+        # if they cannot be provisioned.
+        ray.get(current_placement_group.ready(), timeout=1800)
+
+    # Set the placement group in the parallel config
+    parallel_config.placement_group = current_placement_group
