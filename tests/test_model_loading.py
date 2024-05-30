f46030d5f1b0fc78a89a54ad98a4d5aaaaf2c75f
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-05-30 15:44:07 +0200

    tests: Specify "reason" in `skipif`

--- tests/test_model_loading.py
+++ tests/test_model_loading.py
@@ -25,7 +25,9 @@ def test_load_non_generative_model(model_id, dataset_config, benchmark_config):
     assert model is not None
 
 
-@pytest.mark.skipif(condition=os.getenv("USE_VLLM", "0") != "1")
+@pytest.mark.skipif(
+    condition=os.getenv("USE_VLLM", "0") != "1", reason="Not using VLLM."
+)
 def test_load_generative_model(generative_model_and_tokenizer):
     """Test loading a generative model."""
     model, tokenizer = generative_model_and_tokenizer
