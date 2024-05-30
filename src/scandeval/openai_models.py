7572f56366ea43452d618645fbbf48cc0c42e748
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-05-30 14:11:00 +0200

    debug

--- src/scandeval/openai_models.py
+++ src/scandeval/openai_models.py
@@ -558,7 +558,6 @@ class OpenAIModel:
 
         # TEMP
         logger.info(f"Generated text: {generation_output!r}")
-        breakpoint()
 
         completion_ids = self.tokenizer([generation_output]).input_ids.tolist()
         output = LongTensor(completion_ids)
