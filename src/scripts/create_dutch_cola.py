ee5309e8900345646cbd1c612534b5973613e98b
Dan Saattrup Nielsen <47701536+saattrupdan@users.noreply.github.com>
2024-07-29 18:19:20 +0200

    Update src/scripts/create_dutch_cola.py

--- src/scripts/create_dutch_cola.py
+++ src/scripts/create_dutch_cola.py
@@ -24,17 +24,53 @@ def main() -> None:
     dataset = dataset.map(lambda sample: {"label": label_mapping[sample["label"]]})
     dataset["val"] = dataset.pop("validation")
 
-    # Create dataset ID
+    full_dataset_id = "ScandEval/dutch-cola-full"
     dataset_id = "ScandEval/dutch-cola"
 
     # Remove the dataset from Hugging Face Hub if it already exists
-    try:
-        api = HfApi()
-        api.delete_repo(dataset_id, repo_type="dataset", missing_ok=True)
-    except HTTPError:
-        pass
+    for id in [dataset_id, full_dataset_id]:
+        try:
+            api = HfApi()
+            api.delete_repo(id, repo_type="dataset", missing_ok=True)
+        except HTTPError:
+            pass
 
-    # Push the dataset to the Hugging Face Hub
+    dataset.push_to_hub(full_dataset_id, private=True)
+    
+    # Convert the dataset to a dataframe
+    df = dataset.to_pandas()
+    assert isinstance(df, pd.DataFrame)
+
+    # Create validation split
+    val_size = 256
+    traintest_arr, val_arr = train_test_split(df, test_size=val_size, random_state=4242)
+    traintest_df = pd.DataFrame(traintest_arr, columns=df.columns)
+    val_df = pd.DataFrame(val_arr, columns=df.columns)
+
+    # Create test split
+    test_size = 2048
+    train_arr, test_arr = train_test_split(
+        traintest_df, test_size=test_size, random_state=4242
+    )
+    train_df = pd.DataFrame(train_arr, columns=df.columns)
+    test_df = pd.DataFrame(test_arr, columns=df.columns)
+
+    # Create train split
+    train_size = 256
+    train_df = train_df.sample(train_size, random_state=4242)
+
+    # Reset the index
+    train_df = train_df.reset_index(drop=True)
+    val_df = val_df.reset_index(drop=True)
+    test_df = test_df.reset_index(drop=True)
+
+    # Collect datasets in a dataset dictionary
+    dataset = DatasetDict(
+        train=Dataset.from_pandas(train_df, split=Split.TRAIN),
+        val=Dataset.from_pandas(val_df, split=Split.VALIDATION),
+        test=Dataset.from_pandas(test_df, split=Split.TEST),
+    )
+
     dataset.push_to_hub(dataset_id, private=True)
 
 
