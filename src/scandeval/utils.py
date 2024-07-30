c2fdd4819117bb035dfa3487d9d386474cb15882
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-07-30 12:37:07 +0200

    feat: Remove postfix label name to instruction prompts

--- src/scandeval/utils.py
+++ src/scandeval/utils.py
@@ -761,14 +761,10 @@ def convert_prompt_to_instruction(prompt: str, tokenizer: "Tokenizer") -> str:
         tokenize=False,
     )
 
-    instruction = "\n\n".join(prompt.split("\n\n")[:-1])
-    label_prefix = prompt.split("\n\n")[-1]
-
     instruction_prompt = tokenizer.apply_chat_template(
-        conversation=[dict(role="user", content=instruction)], **chat_template_kwargs
+        conversation=[dict(role="user", content=prompt)], **chat_template_kwargs
     )
     assert isinstance(instruction_prompt, str)
-    instruction_prompt += label_prefix
 
     return instruction_prompt
 
