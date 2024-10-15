c74201125541fdedd322ab5528a5cbc153e3e632
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-10-15 16:39:36 +0200

    chore: Re-enable hooks

--- src/scandeval/model_setups/utils.py
+++ src/scandeval/model_setups/utils.py
@@ -155,10 +155,7 @@ def align_model_and_tokenizer(
             except ValueError as e:
                 if "decoder_input_ids" not in str(e):
                     raise e
-                model(
-                    input_ids=dummy_inputs,
-                    labels=torch.zeros(1, 1).long().to(dummy_inputs.device),
-                )
+                model(input_ids=dummy_inputs, labels=torch.zeros(1, 1).long())
                 break
 
             # This happens if `max_length` is too large
