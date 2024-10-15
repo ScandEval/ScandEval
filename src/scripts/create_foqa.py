f12959f3d0e6d231a98e97a9125935b032cd42da
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-10-15 15:13:24 +0200

    chore: Remove processing of foqa, as it has been done already

--- src/scripts/create_foqa.py
+++ src/scripts/create_foqa.py
@@ -1,12 +1,6 @@
 """Create the FoQA dataset and upload them to the HF Hub."""
 
 import pandas as pd
-from constants import (
-    MAX_NUM_CHARS_IN_CONTEXT,
-    MAX_NUM_CHARS_IN_QUESTION,
-    MIN_NUM_CHARS_IN_CONTEXT,
-    MIN_NUM_CHARS_IN_QUESTION,
-)
 from datasets.arrow_dataset import Dataset
 from datasets.dataset_dict import DatasetDict
 from datasets.load import load_dataset
@@ -30,60 +24,6 @@ def main() -> None:
     assert isinstance(train_df, pd.DataFrame)
     assert isinstance(val_df, pd.DataFrame)
     assert isinstance(test_df, pd.DataFrame)
-
-    # Only work with samples where the context is not very large or small
-    train_lengths = train_df.context.str.len()
-    val_lengths = val_df.context.str.len()
-    test_lengths = test_df.context.str.len()
-    train_df = train_df[
-        train_lengths.between(MIN_NUM_CHARS_IN_CONTEXT, MAX_NUM_CHARS_IN_CONTEXT)
-    ]
-    val_df = val_df[
-        val_lengths.between(MIN_NUM_CHARS_IN_CONTEXT, MAX_NUM_CHARS_IN_CONTEXT)
-    ]
-    test_df = test_df[
-        test_lengths.between(MIN_NUM_CHARS_IN_CONTEXT, MAX_NUM_CHARS_IN_CONTEXT)
-    ]
-
-    # Only work with samples where the question is not very large or small
-    train_question_lengths = train_df.question.str.len()
-    val_question_lengths = val_df.question.str.len()
-    test_question_lengths = test_df.question.str.len()
-    train_df = train_df[
-        train_question_lengths.between(
-            MIN_NUM_CHARS_IN_QUESTION, MAX_NUM_CHARS_IN_QUESTION
-        )
-    ]
-    val_df = val_df[
-        val_question_lengths.between(
-            MIN_NUM_CHARS_IN_QUESTION, MAX_NUM_CHARS_IN_QUESTION
-        )
-    ]
-    test_df = test_df[
-        test_question_lengths.between(
-            MIN_NUM_CHARS_IN_QUESTION, MAX_NUM_CHARS_IN_QUESTION
-        )
-    ]
-    assert isinstance(train_df, pd.DataFrame)
-    assert isinstance(val_df, pd.DataFrame)
-    assert isinstance(test_df, pd.DataFrame)
-
-    # Extract information on which examples contain an answer
-    def has_answer(example: dict) -> bool:
-        return len(example["text"]) > 0 and example["text"][0] != ""
-
-    train_has_answer: pd.Series = train_df.answers.map(has_answer)
-    val_has_answer: pd.Series = val_df.answers.map(has_answer)
-    test_has_answer: pd.Series = test_df.answers.map(has_answer)
-
-    # Only work with the questions having answers in the context
-    train_df = train_df.loc[train_has_answer]
-    val_df = val_df.loc[val_has_answer]
-    test_df = test_df.loc[test_has_answer]
-
-    val_df = val_df.reset_index(drop=True)
-    test_df = test_df.reset_index(drop=True)
-    train_df = train_df.reset_index(drop=True)
 
     # Collect datasets in a dataset dictionary
     dataset = DatasetDict(
