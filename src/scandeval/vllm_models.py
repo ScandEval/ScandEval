f72b5ab0ecb81703e8f21e11b64148260fd8a580
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-10-22 13:23:44 +0200

    fix: Store adapter in separate subdir

--- src/scandeval/vllm_models.py
+++ src/scandeval/vllm_models.py
@@ -159,7 +159,8 @@ class VLLMModel:
 
         if self.adapter_base_model_id is not None:
             self.adapter_path = snapshot_download(
-                repo_id=self.model_config.model_id, cache_dir=self.model_cache_dir
+                repo_id=self.model_config.model_id,
+                cache_dir=Path(self.model_cache_dir) / "adapter",
             )
             self.lora_request = LoRARequest(
                 lora_name="adapter", lora_int_id=1, lora_path=self.adapter_path
