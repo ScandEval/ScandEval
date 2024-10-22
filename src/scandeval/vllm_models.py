73c7433523aa88cf492ba8f2dcd69700d1406492
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-10-22 13:59:38 +0200

    fix: Set max_lora_rank to 1 million

--- src/scandeval/vllm_models.py
+++ src/scandeval/vllm_models.py
@@ -144,6 +144,7 @@ class VLLMModel:
             # we disable it for now
             enable_prefix_caching=False,
             enable_lora=self.adapter_base_model_id is not None,
+            max_lora_rank=1e6,
         )
 
         self._model = self._initialise(vllm_kwargs=vllm_kwargs)
