d366afcbfedc9ffcdcc6759f968cd196a76c8b40
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-10-22 12:16:42 +0200

    debug

--- src/scandeval/model_setups/hf.py
+++ src/scandeval/model_setups/hf.py
@@ -317,13 +317,13 @@ class HFModelSetup:
             and self.benchmark_config.device == torch.device("cuda")
             and os.getenv("USE_VLLM", "1") == "1"
         )
+        breakpoint()
 
         if use_vllm and importlib.util.find_spec("vllm") is None:
             raise NeedsExtraInstalled(extra="generative")
 
         if use_vllm:
             try:
-                breakpoint()
                 model = VLLMModel(
                     model_config=model_config,
                     hf_model_config=config,
