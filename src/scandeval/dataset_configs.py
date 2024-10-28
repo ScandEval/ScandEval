f95857d9310a800fe0024efb6da577f244f708b1
Oliver Kinch <oliver.kinch@gmail.com>
2024-10-28 09:50:40 +0100

    New dataset `ice-ce`

--- src/scandeval/dataset_configs.py
+++ src/scandeval/dataset_configs.py
@@ -688,6 +688,21 @@ DUTCH_COLA_FULL_CONFIG = DatasetConfig(
     unofficial=True,
 )
 
+ICE_EC_CONFIG = DatasetConfig(
+    name="ice-ec",
+    pretty_name="Icelandic Error Corpus",
+    huggingface_id="ScandEval/ice-ec",
+    task=LA,
+    languages=[IS],
+    prompt_prefix="Eftirfarandi eru setningar og hvort þær eru málfræðilega réttar.",
+    prompt_template="Setning: {text}\nMálfræðilega rétt: {label}",
+    prompt_label_mapping=dict(correct="já", incorrect="nei"),
+    instruction_prompt="Setning: {text}\n\nGreinið hvort setningin er málfræðilega "
+    "rétt eða ekki. Svarið skal vera 'já' ef setningin er rétt og 'nei' ef hún er ekki.",
+    num_few_shot_examples=12,
+    max_generated_tokens=1,
+    unofficial=True,
+)
 
 ### READING COMPREHENSION DATASETS ###
 
