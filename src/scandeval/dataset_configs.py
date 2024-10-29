36f43a2e43814e371d9c89c6bac3af65c56ece85
Oliver Kinch <oliver.kinch@gmail.com>
2024-10-29 08:03:52 +0100

    Subset and full dataset for icelandic error corpus

--- src/scandeval/dataset_configs.py
+++ src/scandeval/dataset_configs.py
@@ -690,8 +690,24 @@ DUTCH_COLA_FULL_CONFIG = DatasetConfig(
 
 ICE_EC_CONFIG = DatasetConfig(
     name="ice-ec",
-    pretty_name="Icelandic Error Corpus",
+    pretty_name="the Icelandic Error Corpus (a subset of the dataset)",
     huggingface_id="ScandEval/ice-ec",
+    task=LA,
+    languages=[IS],
+    prompt_prefix="Eftirfarandi eru setningar og hvort þær eru málfræðilega réttar.",
+    prompt_template="Setning: {text}\nMálfræðilega rétt: {label}",
+    prompt_label_mapping=dict(correct="já", incorrect="nei"),
+    instruction_prompt="Setning: {text}\n\nGreinið hvort setningin er málfræðilega "
+    "rétt eða ekki. Svarið skal vera 'já' ef setningin er rétt og 'nei' ef hún er ekki.",
+    num_few_shot_examples=12,
+    max_generated_tokens=1,
+    unofficial=True,
+)
+
+ICE_EC_FULL_CONFIG = DatasetConfig(
+    name="ice-ec-full",
+    pretty_name="the full Icelandic Error Corpus",
+    huggingface_id="ScandEval/ice-ec-full",
     task=LA,
     languages=[IS],
     prompt_prefix="Eftirfarandi eru setningar og hvort þær eru málfræðilega réttar.",
