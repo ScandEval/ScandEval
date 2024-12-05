4d512bf0430ff0d33763bbeeb0c15e03c0268bdd
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-12-05 16:10:36 +0100

    chore: Remove breakpoints

--- src/scandeval/benchmark_modules/vllm.py
+++ src/scandeval/benchmark_modules/vllm.py
@@ -297,7 +297,6 @@ class VLLMModel(HuggingFaceEncoderModel):
                 ]
                 for raw_output in raw_outputs
             ]
-            breakpoint()
             output = GenerativeModelOutput(sequences=completions, scores=scores)
         else:
             output = GenerativeModelOutput(sequences=completions)
