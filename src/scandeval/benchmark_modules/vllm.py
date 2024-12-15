ddf78a32dadf11d196312e281eef531af2072e0c
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-12-15 12:23:40 +0100

    chore: Remove temp comment

--- src/scandeval/benchmark_modules/vllm.py
+++ src/scandeval/benchmark_modules/vllm.py
@@ -513,7 +513,7 @@ class VLLMModel(HuggingFaceEncoderModel):
                 trust_remote_code=self.benchmark_config.trust_remote_code,
                 revision=self.model_config.revision,
                 seed=4242,
-                distributed_executor_backend="ray",  # TEMP?
+                distributed_executor_backend="ray",
                 tensor_parallel_size=torch.cuda.device_count(),
                 disable_custom_all_reduce=True,
                 quantization=quantization,
