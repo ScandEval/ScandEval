1e4961a1bea23c0d0275f8ac75cbf71704cecdea
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-01-06 15:24:09 +0100

    debug

--- src/scandeval/benchmark_modules/vllm.py
+++ src/scandeval/benchmark_modules/vllm.py
@@ -289,7 +289,7 @@ class VLLMModel(HuggingFaceEncoderModel):
             pydantic_class = create_model("AnswerFormat", **keys_and_their_types)
             schema = pydantic_class.model_json_schema()
             guided_decoding = GuidedDecodingParams(
-                json=schema, backend="outlines", whitespace_pattern=r" ?"
+                json=schema, backend="xgrammar", whitespace_pattern=r" ?"
             )
         else:
             guided_decoding = None
