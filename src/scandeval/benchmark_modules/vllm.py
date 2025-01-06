59eaea20004a2531f4035e67e438dfc3819f3745
Dan Saattrup Nielsen <47701536+saattrupdan@users.noreply.github.com>
2025-01-06 17:15:24 +0100

    Fix/ner oom (#689)
    * debug
    
    * debug
    
    * debug: Do not block terminal output
    
    * debug
    
    * fix: Remove max 5 limit in NER JSON schema
    
    * fix: Use xgrammar as guided_decoding_backend
    
    * feat: Use xgrammar with max_length=5, even though it does not work yet
    
    * chore: Re-enable hook
    
    * feat: Specify max 5 entities of each type in instruction prompts
    
    * fix: Specify max 5 in prompt prefix of SUC3_CONFIG
    
    * chore: Revert all prompt changes re max 5 ner
    
    * chore: Back to outlines
    
    * fix: Use list[str] in guided decoding json schema
    
    * docs: Remove xgrammar comment
    
    * docs: Update changelog
    
    * fix: Add max 5 requirement back in
    
    * debug
    
    * fix: Set whitespace_pattern
    
    * debug
    
    * debug
    
    * chore: Use outlines
    
    * fix: Remove whitespace_pattern
    
    * chore: Add whitespace_pattern back in
    
    * fix: Downgrade vllm
    
    * chore: Re-enable hook
    
    * docs: Update changelog
    
    * fix: Downgrade vllm

--- src/scandeval/benchmark_modules/vllm.py
+++ src/scandeval/benchmark_modules/vllm.py
@@ -278,13 +278,19 @@ class VLLMModel(HuggingFaceEncoderModel):
         if self.dataset_config.task.name in TASKS_USING_JSON:
             ner_tag_names = list(self.dataset_config.prompt_label_mapping.values())
 
+            # Xgrammar, the guided decoding backend, does not yet support a maximal
+            # amount of items in a list, so the `max_length` parameter will be ignored
+            # in practice. However, they're working on implementing this feature, so
+            # we include it here for future compatibility.
             keys_and_their_types: dict[str, t.Any] = {
                 tag_name: (conlist(str, max_length=5), ...)
                 for tag_name in ner_tag_names
             }
             pydantic_class = create_model("AnswerFormat", **keys_and_their_types)
             schema = pydantic_class.model_json_schema()
-            guided_decoding = GuidedDecodingParams(json=schema)
+            guided_decoding = GuidedDecodingParams(
+                json=schema, backend="outlines", whitespace_pattern=r" ?"
+            )
         else:
             guided_decoding = None
 
@@ -862,7 +868,6 @@ def load_model_and_tokenizer(
             enable_prefix_caching=False,
             enable_lora=model_config.adapter_base_model_id is not None,
             max_lora_rank=256,
-            guided_decoding_backend="outlines",
         )
     except ValueError as e:
         if "trust_remote_code" in str(e):
